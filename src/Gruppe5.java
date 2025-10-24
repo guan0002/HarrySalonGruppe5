5c5be1e7e9e4471583d8d2541d6fcf38c9e5b18e
guan0002 <guan0002@stud.ek.dk>
2025-10-24 12:04:40 +0200

    Slettede alt unødvendigt i programmet

--- src/Gruppe5.java
+++ src/Gruppe5.java
@@ -1,17 +1,5 @@
-import java.util.Scanner;
-
 public class Gruppe5 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-       Scanner scanner = new Scanner(System.in);
 
-       String hilsen;
-
-        System.out.println("Hello til hvem?");
-        hilsen = scanner.nextLine();
-
-        System.out.println("Nåårh, okay...");
-        scanner.close();
-        int tal = 1;
     }
 }
